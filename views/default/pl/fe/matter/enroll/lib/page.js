d9bf28b6d14c8e1545ac388eddb997fb9a15c900
“lishuai <1076283698@qq.com>
2017-05-25 10:35:03 +0800

    1.修改引用参数不正确的bug;2.更改页面显示形式,添加完描述后自动切换到删除,删除后自动切换到删除

--- views/default/pl/fe/matter/enroll/lib/page.js
+++ views/default/pl/fe/matter/enroll/lib/page.js
@@ -152,11 +152,11 @@ define(['wrap'], function(SchemaWrap) {
             $html = $('<div>' + this.html + '</div>');
             if ($dom = $html.find("[schema='" + oSchema.id + "']")) {
                 //增加一个描述。如果没有添加
-                if(schema.descriptionShoworhide){
+                if(oSchema.descriptionShoworhide){
                     if(!$dom.find('[class="description"]').length){
-                        $('<div class="description">' + schema.description + '</div>').insertAfter($dom.find('label')[0])
+                        $('<div class="description">' + oSchema.description + '</div>').insertAfter($dom.find('label')[0])
                     }else{
-                        $dom.find('[class="description"]').html(schema.description);
+                        $dom.find('[class="description"]').html(oSchema.description);
                     }
                 }else{//移除时使用
                     $dom.find('[class="description"]')[0].remove();
